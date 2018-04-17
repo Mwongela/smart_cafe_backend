382ac2a70082801bff0cfa3c1662280cbcd0f79b
Stephen Mwongela <stephenmwongela@gmail.com>
2018-04-17 22:57:32 +0300

    Finished on login page design

--- smart_cafe/urls.py
+++ smart_cafe/urls.py
@@ -13,14 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from smart_cafe_ui import urls as ui_urls
+from django.contrib.auth import views as auth_views
 
 from smart_cafe import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', include(ui_urls))
+                  path('', include(ui_urls), name='ui'),
+                  path('accounts/login/', auth_views.login, name="login"),
+                  path('accounts/logout/', auth_views.logout_then_login, name='logout'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
